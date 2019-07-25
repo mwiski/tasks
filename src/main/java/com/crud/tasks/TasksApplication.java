55633edcdc4994b791ec22f2019395727b8b7a8e
mwiski <wiskim1@gmail.com>
2019-07-25 20:20:28 +0200

    adding all files

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
 //public class TasksApplication extends SpringBootServletInitializer {
+
+@SpringBootApplication
 public class TasksApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TasksApplication.class, args);
