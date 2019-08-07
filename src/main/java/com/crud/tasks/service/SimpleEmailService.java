d5acbcaff9889d0cddd264570297237190c1ba56
mwiski <wiskim1@gmail.com>
2019-08-07 20:29:00 +0200

    KOD-23.3: not null condition

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -32,7 +32,7 @@ public class SimpleEmailService {
         mailMessage.setTo(mail.getMailTo());
         mailMessage.setSubject(mail.getSubject());
         mailMessage.setText(mail.getMessage());
-        if (!mail.getToCc().isEmpty()) {
+        if (mail.getToCc() != null && !mail.getToCc().isEmpty()) {
             mailMessage.setCc(mail.getToCc());
         }
         return mailMessage;
