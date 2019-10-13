74995906345ed52d4fd3697d00cc6987cbfaf942
mwiski <wiskim1@gmail.com>
2019-10-13 22:27:52 +0200

    KOD-29: Spring WEB - Rest API 3

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -3,12 +3,10 @@ package com.crud.tasks.controller;
 import com.crud.tasks.domain.CreatedTrelloCardDto;
 import com.crud.tasks.domain.TrelloBoardDto;
 import com.crud.tasks.domain.TrelloCardDto;
-import com.crud.tasks.service.TrelloService;
 import com.crud.tasks.trello.facade.TrelloFacade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @CrossOrigin("*")
 @RestController
--- src/main/java/com/crud/tasks/trello/validator/TrelloValidator.java
+++ src/main/java/com/crud/tasks/trello/validator/TrelloValidator.java
@@ -5,7 +5,6 @@ import com.crud.tasks.trello.facade.TrelloFacade;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-
 import java.util.List;
 import java.util.stream.Collectors;
 
